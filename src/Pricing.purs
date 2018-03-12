927d20e26af0e6768e376f2fd14b83afc1c83989
Philippe Vinchon <pvinchon@gilt.com>
2018-03-12 18:15:57 +0000

    PureScript mapping for aws-sdk-js 2.207.0

--- src/Pricing.purs
+++ src/Pricing.purs
@@ -20,22 +20,26 @@ import Data.StrMap as StrMap
 import AWS.Request as Request
 import AWS.Request.Types as Types
 
-serviceName = "Pricing" :: String
-
 
 -- | <p>Returns the metadata for one service or a list of the metadata for all services. Use this without a service code to get the service codes for all services. Use it with a service code, such as <code>AmazonEC2</code>, to get information specific to that service, such as the attribute names available for that service. For example, some of the attribute names available for EC2 are <code>volumeType</code>, <code>maxIopsVolume</code>, <code>operation</code>, <code>locationType</code>, and <code>instanceCapacity10xlarge</code>.</p>
 describeServices :: forall eff. DescribeServicesRequest -> Aff (exception :: EXCEPTION | eff) DescribeServicesResponse
-describeServices = Request.request serviceName "describeServices" 
+describeServices = Request.request service method  where
+    service = Request.ServiceName "Pricing"
+    method = Request.MethodName "describeServices"
 
 
 -- | <p>Returns a list of attribute values. Attibutes are similar to the details in a Price List API offer file. For a list of available attributes, see <a href="http://docs.aws.amazon.com/awsaccountbilling/latest/aboutv2/reading-an-offer.html#pps-defs">Offer File Definitions</a> in the <a href="http://docs.aws.amazon.com/awsaccountbilling/latest/aboutv2/billing-what-is.html">AWS Billing and Cost Management User Guide</a>.</p>
 getAttributeValues :: forall eff. GetAttributeValuesRequest -> Aff (exception :: EXCEPTION | eff) GetAttributeValuesResponse
-getAttributeValues = Request.request serviceName "getAttributeValues" 
+getAttributeValues = Request.request service method  where
+    service = Request.ServiceName "Pricing"
+    method = Request.MethodName "getAttributeValues"
 
 
 -- | <p>Returns a list of all products that match the filter criteria.</p>
 getProducts :: forall eff. GetProductsRequest -> Aff (exception :: EXCEPTION | eff) GetProductsResponse
-getProducts = Request.request serviceName "getProducts" 
+getProducts = Request.request service method  where
+    service = Request.ServiceName "Pricing"
+    method = Request.MethodName "getProducts"
 
 
 newtype AttributeNameList = AttributeNameList (Array String)
